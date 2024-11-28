c4ee6cdc8fa165b8b4a9a57a21b5bc7ba6c00562
Dev <42912984+thedev09@users.noreply.github.com>
2024-11-28 11:42:50 +0530

    Create commands.js

---
+++ v1/commands.js
@@ -0,0 +1,41 @@
+export const commands = {
+  help: `Available commands:
+  - about: Learn about me
+  - skills: See my technical skills
+  - projects: View my projects
+  - contact: Get my contact info
+  - socials: Social media links
+  - clear: Clear terminal
+  - theme: Change color theme`,
+
+  about: `Hi! I'm Dev, a passionate Full Stack Developer.
+I craft digital experiences and love exploring new technologies.
+Currently working on exciting projects and always learning something new.`,
+
+  skills: [
+    'Frontend: React, JavaScript/TypeScript, HTML/CSS',
+    'Backend: Node.js, Express',
+    'Database: MongoDB, SQL',
+    'Tools: Git, Docker, AWS',
+    'Other: UI/UX, Responsive Design'
+  ],
+
+  projects: [
+    {
+      name: 'Personal Website',
+      tech: 'HTML, CSS, JavaScript',
+      desc: 'My portfolio website with multiple versions',
+      link: 'https://github.com/thedev09'
+    }
+    // Add more projects here
+  ],
+
+  contact: `Email: thakurrahul2019@gmail.com
+Location: India
+Open for opportunities: Yes`,
+
+  socials: `GitHub: https://github.com/thedev09
+Twitter: https://twitter.com/thakur_d09
+Facebook: https://www.facebook.com/sharifladka69
+Instagram: https://www.instagram.com/thedev09.eth`
+};
