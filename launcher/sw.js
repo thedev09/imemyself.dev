003869836036258a5fea67170bfe0ef58520e001
Dev <thakurrahul2019@gmail.com>
2025-07-09 17:12:24 +0530

    m

---
+++ launcher/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'my-apps-launcher-v1';
+const urlsToCache = [
+  '/launcher/',
+  '/launcher/index.html',
+  '/launcher/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
